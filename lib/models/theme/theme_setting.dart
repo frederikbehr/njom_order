f709aa178940d4166d3062670884ae5379a2d1f4
fred077a <fred18q3@stud.kea.dk>
2024-12-13 04:17:25 +0100

    new colors

--- lib/models/theme/theme_setting.dart
+++ lib/models/theme/theme_setting.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ThemeSetting {
-  final Color primaryColor = Colors.blue.shade900;
-  final Color secondary = const Color(0xffA1670D);
+  //Color pallete
+  final Color primaryColor = const Color(0xff254441);
+  final Color secondary = const Color(0xff7CAE7A);
+
+  //Fills
   final Color background = const Color(0xffededed);
   final Color dialog = const Color(0xffeeeeee);
+
+  //Text
   final Color titleOnColor = Colors.white;
-  final Color titleOnBackground = Colors.black;
+  final Color titleOnBackground = Colors.black87;
   final Color bodyOnBackground = Colors.black54;
+
+  //Style
+  final Color shadow = Colors.black12;
+  final Color border = Colors.black87;
+
 }
